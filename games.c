c718caa248500769b11755bf1d3b9d551526e19a
Arturo Soberanes <ArturoSoberanes@Arturo-Soberanes.local>
2020-11-17 00:21:17 -0600

    Create games.c
    1

---
+++ games.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+  int i=0, opt;
+  do
+  {
+    printf("Bienvenido a XGame.\nPor favor escoja que juego desea iniciar.\n");
+    printf("1.Sudoku\n2.Gato\n3.Laberinto");
+    switch(opt)
+    {
+      case 1:
+
+      break;
+      case 2:
+
+      break;
+      case 3:
+
+      break;
+      default:
+
+    }
+  }while(i>1);
+}
