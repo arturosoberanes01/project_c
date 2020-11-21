2c57f215ff71bb85c2b582b452e3dc2048fdc61c
arturosoberanes01 <48600213+arturosoberanes01@users.noreply.github.com>
2020-11-20 21:00:16 -0600

    6

--- games.c
+++ games.c
@@ -5,21 +5,26 @@ int main(){
   do
   {
     printf("Bienvenido a XGame.\nPor favor escoja que juego desea iniciar.\n");
-    printf("1.Sudoku\n2.Gato\n3.Laberinto");
+    printf("1.Sudoku\n2.Gato\n3.Laberinto\n4.Salir\n");
+    scanf("\n%d",&opt);
     switch(opt)
     {
       case 1:
-
+        printf("SUDOKU\n");
       break;
       case 2:
-
+      printf("GATO\n");
       break;
       case 3:
-
+      printf("LABERINTO\n");
+      break;
+      case 4:
+      i=3;
       break;
       default:
-
+      printf("Opción no válida\n");
     }
-  }while(i>1);
+  }while(i<1);
+  printf("Muchas gracias por usar XGame\n");
   return 0;
 }
